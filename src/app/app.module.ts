1bddd1f78f4c5f164eece227336bac15e7db9f52
Daniela Romero <danyrp45@gmail.com>
2023-01-09 13:04:31 -0600

    add menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 
 
@@ -13,7 +14,8 @@ import { PrimeNgModule } from './prime-ng/prime-ng.module';
   ],
   imports: [
     BrowserModule,
-    PrimeNgModule
+    PrimeNgModule,
+    SharedModule
     
   ],
   providers: [],
