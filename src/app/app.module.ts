7a1f1139bbdee68fed8b554f45e6c6380c517a35
Daniela Romero <danyrp45@gmail.com>
2023-01-09 11:31:55 -0600

    add prime Ng for css components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//PrimeNg
+import {ButtonModule} from 'primeng/button';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
