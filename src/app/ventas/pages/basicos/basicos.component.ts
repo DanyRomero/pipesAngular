6e2e86c361f75357d56c4d9225e7156dbba01fb5
Daniela Romero <danyrp45@gmail.com>
2023-01-09 16:57:02 -0600

    add cards for basics pipes

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class BasicosComponent {
 
+  nombreLower: string = 'daniela';
+  nombreUpper: string = 'DANIELA';
+  nombreCompleto: string = 'daniEla ROmero';
 }
