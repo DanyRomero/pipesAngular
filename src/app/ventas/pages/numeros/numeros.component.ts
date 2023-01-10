f11867eae4cf10353ba4756866a214f85a75c571
Daniela Romero <danyrp45@gmail.com>
2023-01-10 11:28:19 -0600

    add currency and percent pipe

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
-  styleUrls: ['./numeros.component.css']
 })
 export class NumerosComponent {
+  ventasNetas : number = 2457393.6474;
+  porcentaje : number = 0.4856;
 
 }
--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
-  styleUrls: ['./ordenar.component.css']
 })
 export class OrdenarComponent {
 
