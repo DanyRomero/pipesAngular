0d07388b610a9830cf40b9dc1c9671ab1249d960
Daniela Romero <danyrp45@gmail.com>
2023-01-10 12:24:26 -0600

    implements i18nSelect and i18nPlural

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
 
-  
+  //i18nSelect
+  nombre: string = 'Fernando';
+  genero: string = 'masculino';
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
+  }
+
+  //i18nPlural
+  clientes: string[] =['Maria', 'Pedro', 'Juan', 'Luis']
+  clientesMapa = {
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando'
+    
+  }
+
+
+
 }
